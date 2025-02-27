9d9f45d409a1288fae3c0ec258874b43e887761b
Adrian Kotowski <adekkotowski1@wp.pl>
2025-02-27 16:57:07 +0100

    Add class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -32,7 +32,12 @@ function titleClickHandler(event) {
     console.log(articleSelector)
     /* find the correct article using the selector (value of 'href' attribute) */
 
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+
     /* add class 'active' to the correct article */
+    
+    targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
